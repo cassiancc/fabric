Sinytra/FAPI-Mojmaps

Fix overriding vanilla translations (#4187)
(cherry picked from commit e82f21f7e92545027a4dec8081c1bcac860a8259)

fabric-resource-loader-v0/src/main/java/net/fabricmc/fabric/mixin/resource/loader/server/LanguageMixin.java
@@ -33,6 +33,8 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import net.fabricmc.fabric.impl.resource.loader.ServerLanguageUtil;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
 import net.minecraft.locale.Language;
 
 @Mixin(Language.class)
@@ -50,6 +52,18 @@ class LanguageMixin {
 		}
 
 		return ImmutableMap.copyOf(map);
+	}
+
+	@Redirect(method = "parseTranslations(Ljava/util/function/BiConsumer;Ljava/lang/String;)V", at = @At(value = "INVOKE", target = "Ljava/lang/Class;getResourceAsStream(Ljava/lang/String;)Ljava/io/InputStream;"))
+	private static InputStream readCorrectVanillaResource(Class instance, String path) throws IOException {
+		ModContainer mod = FabricLoader.getInstance().getModContainer("minecraft").orElseThrow();
+		Path langPath = mod.findPath(path).orElse(null);
+
+		if (langPath == null) {
+			throw new IOException("Could not read %s from minecraft ModContainer".formatted(path));
+		} else {
+			return Files.newInputStream(langPath);
+		}
 	}
 
 	private static void loadFromPath(Path path, BiConsumer<String, String> entryConsumer) {

cassiancc/fabric

fabric-resource-loader-v0/src/main/java/net/fabricmc/fabric/mixin/resource/loader/server/LanguageMixin.java
@@ -36,6 +36,8 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import net.minecraft.util.Language;
 
 import net.fabricmc.fabric.impl.resource.loader.ServerLanguageUtil;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
 
 @Mixin(Language.class)
 class LanguageMixin {
@@ -52,6 +54,18 @@ class LanguageMixin {
 		}
 
 		return ImmutableMap.copyOf(map);
+	}
+
+	@Redirect(method = "load(Ljava/util/function/BiConsumer;Ljava/lang/String;)V", at = @At(value = "INVOKE", target = "Ljava/lang/Class;getResourceAsStream(Ljava/lang/String;)Ljava/io/InputStream;"))
+	private static InputStream readCorrectVanillaResource(Class instance, String path) throws IOException {
+		ModContainer mod = FabricLoader.getInstance().getModContainer("minecraft").orElseThrow();
+		Path langPath = mod.findPath(path).orElse(null);
+
+		if (langPath == null) {
+			throw new IOException("Could not read %s from minecraft ModContainer".formatted(path));
+		} else {
+			return Files.newInputStream(langPath);
+		}
 	}
 
 	private static void loadFromPath(Path path, BiConsumer<String, String> entryConsumer) {